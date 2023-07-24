552e497213884089e573e3be4fdc57a3b4ff7d9b
Vladislav2097 <116560621+Vladislav2097@users.noreply.github.com>
2023-07-24 16:17:48 +0300

    Refactor

---
+++ src/pages/About.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <h1>
+            Это приложение создано...Просто потому-что...
+        </h1>
+    );
+};
+
+export default About;
